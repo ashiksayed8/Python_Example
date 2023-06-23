9a345996888b27e6c69fa46f6051a48349697ed2
ashik-islam <ashiksayed8@gmail.com>
2023-06-23 10:03:34 +0600

    Fahrenheit , celsius convert

---
+++ convert_celsius_to_fahrenheit.py
@@ -0,0 +1,18 @@
+""" #Python Program to convert temperature in celsius to fahrenheit 
+
+#Change this value for a different result 
+celsius = 37.5
+
+#Calculate fahrenheit
+farhrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius, farhrenheit)) """
+
+#Python Program to convert temperature in fahrenheit to celsius-
+#Change this value for a different result
+
+farhenheit = 98 
+
+#Calculate celsius----------
+
+celsius = (farhenheit - 32) / 1.8
+print('%0.1f degree Fahrenheit is equal %0.1f degree Celsius', (farhenheit, celsius))
