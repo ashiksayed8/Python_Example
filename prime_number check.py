46742c924a943e4531b41538a595386129b7a25f
ashik-islam <ashiksayed8@gmail.com>
2023-06-24 17:00:52 +0600

    prime number

--- prime_number check.py
+++ prime_number check.py
@@ -1,4 +1,4 @@
-# Program to check if a number is prime or not--------
+""" # Program to check if a number is prime or not--------
 
 num = 28
 
@@ -22,4 +22,28 @@ elif num > 1:
     if flag:
         print(num, "is not prime number")
     else:
-        print(num, "is a prime number")
+        print(num, "is a prime number") """
+        
+        
+        
+num = 407
+
+# To take input from the user
+#num = int(inpput("Enter a number: "))
+
+if num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    #check for factors
+    for i in range(2, num):
+        if(num % i) == 0:
+            print(num, "is not a prime number")
+            print(i, "times", num // i, "is", num)
+            break
+        else:
+            print(num, "is a prime number")
+# if input number is less than
+# or equal to 1, it is not prime
+
+else:
+    print(num, "is not a prime number")
