237ab2e7c3cb0c6af7acb6170935984c861e7631
ashik-islam <ashiksayed8@gmail.com>
2023-06-30 15:35:50 +0600

    filter function using

---
+++ find_number_division_another_number.py
@@ -0,0 +1,10 @@
+# Take a list of numbers
+
+my_list = [12, 65, 54, 39, 102, 339, 221,169]
+
+#use anonymous function to filter
+
+result = list(filter(lambda x: (x % 13 == 0), my_list))
+
+# display the reult 
+print("Numbers divisible by 13 are", result)
