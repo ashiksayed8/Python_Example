d9a56dbbbd704a9babdfb309935b259f6bfaff71
ashik-islam <ashiksayed8@gmail.com>
2023-07-01 11:47:25 +0600

    Binary octal hexadecimal

---
+++ binary_octal_hexadecimal.py
@@ -0,0 +1,9 @@
+# Python program to convert decimal into othe number system
+
+dec = 344
+
+print("The decimal value of", dec, "is:")
+
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal")
+print(hex(dec), "in hexadecimal")
